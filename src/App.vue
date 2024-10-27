b41a9a1eed67e0168d55b75bea0f4fc72bc41ddd
Joery <44531907+Joery-M@users.noreply.github.com>
2024-10-27 15:34:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <table style="width: 1000px">
+        <thead>
+            <th>Template with .value</th>
+            <th>Template without .value</th>
+            <th>v-model without .value</th>
+            <th>With .value</th>
+        </thead>
+        <tr v-for="item in items" :key="item.id">
+            <!-- Extension marks this as incorrect -->
+            <td>{{ item.name.value }}</td>
+            <td>
+                <!-- Extension marks this as correct -->
+                {{ item.name }}
+                <!-- Runtime doesn't mind, shows it correctly -->
+            </td>
+
+            <!-- But this is where the trouble begins -->
+
+            <td>
+                <!-- What the extension expects -->
+                <input type="text" v-model="item.name" />
+                <!-- Editing this field breaks the application -->
+            </td>
+
+            <td>
+                <!-- What is actually correct -->
+                <input type="text" v-model="item.name.value" />
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script setup lang="ts">
+import { ref, shallowReactive, type Ref } from "vue";
+
+interface MyItem {
+    name: Ref<string>;
+    id: string;
+}
+
+const items = shallowReactive<MyItem[]>([
+    {
+        name: ref("test"),
+        id: crypto.randomUUID(),
+    },
+]);
+</script>
+
+<style>
+table,
+th,
+td {
+    border-collapse: collapse;
+    border: 1px solid;
+}
+</style>
